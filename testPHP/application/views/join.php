d8afdea56e34048f6c0697aa429a9c431dd442ef
DonPablo <paserv@gmail.com>
2015-04-21 12:31:26 +0200

    update

---
+++ testPHP/application/views/join.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>One Million Social Club</title>
+<script type="text/javascript" src="../public/js/fusion.js"></script>
+<script type="text/javascript" src="../public/js/config.js"></script>
+<script type="text/javascript" src="../public/js/jquery-2.1.3.min.js"></script>
+<link href="../public/css/omsc.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+ 	<a href="views/join.php"><img src="../public/img/join_us.png"></a> 
+ 
+ </body>
+</html>
