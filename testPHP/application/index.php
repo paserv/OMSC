e158d185bc01fe1b3aa47015b1d89418975822ae
Paolo <Paolo@Paolo-Notebook>
2015-04-22 22:47:15 +0200

    update

--- testPHP/application/index.php
+++ testPHP/application/index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>One Million Social Club</title>
-<script type="text/javascript" src="../public/js/fusion.js"></script>
+<script type="text/javascript" src="../public/js/index.js"></script>
 <script type="text/javascript" src="../public/js/config.js"></script>
 <script type="text/javascript" src="../public/js/jquery-2.1.3.min.js"></script>
 <link href="../public/css/omsc.css" rel="stylesheet" type="text/css">
@@ -9,7 +9,7 @@
 <body>
 
 	<div id="map-canvas"></div>
- 	<a href="views/join_choose.php"><img src="../public/img/join_us.png"></a> 
+ 	<a href="views/social_choose.php"><img src="../public/img/join_us.png"></a> 
  
  </body>
 </html>
