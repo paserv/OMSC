fc7fced95a7a0d9be8d2e92d6484f96bdb449eab
Paolo <Paolo@Paolo-Notebook>
2015-04-23 23:31:10 +0200

    update

---
+++ testPHP/application/dto/DBUser.php
@@ -0,0 +1,25 @@
+<?php
+include_once 'SocialUser.php';
+
+class DBUser extends SocialUser {
+	public $latitude;
+	public $longitude;
+	public $description;
+	public $timestamp;
+	
+	function __construct($id, $name, $email, $latitude, $longitude, $description, $socialPageUrl, $avatarUrl, $timestamp, $socialNetwork) {
+		
+		$this->socialId = $id;
+		$this->name = $name;
+		$this->email = $email;
+		$this->longitude = $longitude;
+		$this->latitude = $latitude;
+		$this->description = $description;
+		$this->socialPageUrl = $socialPageUrl;
+		$this->avatarUrl = $avatarUrl;
+		$this->timestamp = $timestamp;
+		$this->socialNetwork = $socialNetwork;
+	}
+	
+}
+?>
