dd89cdebe2a3647449d4270293eff8da16f7a2f9
DonPablo <paserv@gmail.com>
2015-04-24 10:01:41 +0200

    update

--- testPHP/application/views/social_choose.php
+++ testPHP/application/views/social_choose.php
@@ -8,9 +8,9 @@
 <html>
 <head>
 <title>Join the One Million Social Club</title>
+<script type="text/javascript" src="../public/js/jquery-2.1.3.min.js"></script>
 <script type="text/javascript" src="../public/js/fusion.js"></script>
 <script type="text/javascript" src="../public/js/config.js"></script>
-<script type="text/javascript" src="../public/js/jquery-2.1.3.min.js"></script>
 <link href="../public/css/omsc.css" rel="stylesheet" type="text/css">
 </head>
 <body>
