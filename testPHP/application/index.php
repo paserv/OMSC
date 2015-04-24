9fe2c7102b22e87852fdc6c8698d00b33eafc91c
DonPablo <paserv@gmail.com>
2015-04-24 08:39:23 +0200

    update

--- testPHP/application/index.php
+++ testPHP/application/index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
 <title>One Million Social Club</title>
-<script type="text/javascript" src="../public/js/index.js"></script>
-<script type="text/javascript" src="../public/js/config.js"></script>
 <script type="text/javascript" src="../public/js/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="../public/js/config.js"></script>
+<script type="text/javascript" src="../public/js/index.js"></script>
 <link href="../public/css/omsc.css" rel="stylesheet" type="text/css">
 </head>
 <body>
