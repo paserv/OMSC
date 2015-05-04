a0748f951837c5e1beed90821c01b95f29eb4147
Paolo <Paolo@Paolo-Notebook>
2015-05-04 23:13:15 +0200

    update

--- testPHP/application/configuration/DBConfig.php
+++ testPHP/application/configuration/DBConfig.php
@@ -4,8 +4,8 @@
 #define ( 'DB_PASSWORD', 'fusion' ); // DB password
 #define ( 'DB_DATABASE', 'fusion' ); // DB name
 
-define ( 'DB_SERVER', 'localhost' );
-define ( 'DB_USERNAME', 'fusion' ); // DB username
-define ( 'DB_PASSWORD', 'fusion' ); // DB password
-define ( 'DB_DATABASE', 'fusion' ); // DB name
+define ( 'DB_SERVER', 'sql.aoapao.com' );
+define ( 'DB_USERNAME', 'aoapaoco77530' ); // DB username
+define ( 'DB_PASSWORD', 'aoapaoco77530' ); // DB password
+define ( 'DB_DATABASE', 'aoap88537' ); // DB name
 ?>
