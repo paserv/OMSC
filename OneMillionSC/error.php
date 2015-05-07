684195d4a337da845a752c06224fc0bd50ddaa92
DonPablo <paserv@gmail.com>
2015-05-07 15:26:47 +0200

    update

---
+++ OneMillionSC/error.php
@@ -0,0 +1,29 @@
+<div id="error">
+	<?php
+	$codes = array(
+			400 => array('400 Bad Request', 'The request cannot be fulfilled due to bad syntax.'),
+			403 => array('403 Forbidden', 'The server has refused to fulfil your request.'),
+			404 => array('404 Not Found', 'The page you requested was not found on this server.'),
+			405 => array('405 Method Not Allowed', 'The method specified in the request is not allowed for the specified resource.'),
+			408 => array('408 Request Timeout', 'Your browser failed to send a request in the time allowed by the server.'),
+			500 => array('500 Internal Server Error', 'The request was unsuccessful due to an unexpected condition encountered by the server.'),
+			502 => array('502 Bad Gateway', 'The server received an invalid response while trying to carry out the request.'),
+			504 => array('504 Gateway Timeout', 'The upstream server failed to send a request in the time allowed by the server.'),
+	);
+	
+	if(isset($_SESSION ["error_code"]) && $_SESSION ["error_code"] !== false){
+		$error_code = $_SESSION ['error_code'];
+		$title = $codes[$error_code][0];
+		$message = $codes[$error_code][1];
+		if ($title != false) {
+			echo '<br><br><br><br><br><h1>'.$title.'</h1>
+			<p>'.$message.'</p>';
+		} else {
+			echo "No Code for Error found";
+		}
+		
+	} else {
+		echo "No Error to Display";
+	}
+	?>	
+</div>
--- OneMillionSC/search.php
+++ OneMillionSC/search.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!doctype html>
 <head>
 <title>One Million Social Club - Search People</title>
@@ -10,42 +11,66 @@
 <link href="public/css/omsc.css" rel="stylesheet">
 <script type="text/javascript">
 var markers = [];
+</script>
+<script type="text/javascript">
 <?php
-require_once 'autoload.php';
-loginRegister_autoload();
-
+$_SESSION ["error_code"] = false;
 if(isset($_GET['query'])){
-	$controller = new Controller();
-	$results = $controller->searchByName($_GET['query']);
-	if ($results !== null) {
-		foreach ($results as $currUser) {
-		?>
-		markers.push({
-	        latitude: "<?php echo $currUser->latitude; ?>",
-	        longitude: "<?php echo $currUser->longitude; ?>",
-	        name: "<?php echo $currUser->name; ?>",
-	        description: "<?php echo $currUser->description; ?>",
-	        timestamp: "<?php echo $currUser->timestamp; ?>",
-	        socialId: "<?php echo $currUser->socialId; ?>",
-	        email: "<?php echo $currUser->email; ?>",
-	        socialPageUrl: "<?php echo $currUser->socialPageUrl; ?>",
-	        avatarUrl: "<?php echo $currUser->avatarUrl; ?>",
-	        socialNetwork: "<?php echo $currUser->socialNetwork; ?>",
-	    });
-	    <?php 
-		}		
+
+	if (isset($_SESSION ["numSearch"])) {
+		$_SESSION ["numSearch"]	= $_SESSION ["numSearch"] + 1;
 	} else {
-		echo "No results found";
-		}
+		$_SESSION ["numSearch"] = 1;
+		$numSearch = $_SESSION ["numSearch"];
 	}
+
+	if ($_SESSION ["numSearch"] <= 3) {
+		require_once 'autoload.php';
+		loginRegister_autoload();
+		$controller = new Controller();
+		$results = $controller->searchByName($_GET['query']);
+		if ($results !== null) {
+			foreach ($results as $currUser) {
+				?>
+				markers.push({
+			        latitude: "<?php echo $currUser->latitude; ?>",
+			        longitude: "<?php echo $currUser->longitude; ?>",
+			        name: "<?php echo $currUser->name; ?>",
+			        description: "<?php echo $currUser->description; ?>",
+			        timestamp: "<?php echo $currUser->timestamp; ?>",
+			        socialId: "<?php echo $currUser->socialId; ?>",
+			        email: "<?php echo $currUser->email; ?>",
+			        socialPageUrl: "<?php echo $currUser->socialPageUrl; ?>",
+			        avatarUrl: "<?php echo $currUser->avatarUrl; ?>",
+			        socialNetwork: "<?php echo $currUser->socialNetwork; ?>",
+			    });
+			    <?php 
+				}		
+			} else {
+				$_SESSION ["error_code"] = 400;
+				}
+			}
+			else {
+				$_SESSION ["error_code"] = 403;
+			}
+	} else {
+		$_SESSION ["error_code"] = 404;
+	}
+	
 ?>
 </script>
 
 </head>
 <body>
-<?php include 'header.php'; ?>
-	
-		<div id="map-canvas" class="corpo"></div>
-<?php include 'footer.php'; ?>
+<?php 
+include 'header.php'; 
+if ($_SESSION ["error_code"]) {
+	include 'error.php';
+} else {
+?>
+	<div id="map-canvas" class="corpo"></div>
+<?php 
+} 
+include 'footer.php'; ?>
 </body>
 </html>
