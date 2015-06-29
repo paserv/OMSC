55a398d8d93801f3e6c7046a6865543ece140daf
DonPablo <paserv@gmail.com>
2015-06-29 16:19:28 +0200

    a

---
+++ OneMillionSC/footer.php
@@ -0,0 +1,15 @@
+	<footer class="page-footer blue darken-4 footer">
+		<div class="footer-copyright container-footer" style="padding:0 20px 0 20px">
+			<div class="grey-text text-lighten-4 left">Total members: 
+			<?php
+			if (isset ( $_SESSION ["total_users"] )) {
+				echo ($_SESSION ["total_users"]);
+			}
+			?> 
+			</div>
+			<a class="grey-text text-lighten-4 right share-img-footer" href="https://plus.google.com/share?url=http://www.aoapao.com" target="_blank"><img src="public/img/plus_share.png"></a>
+			<a class="grey-text text-lighten-4 right share-img-footer" href="https://twitter.com/intent/tweet?url=http://aoapao.com" target="_blank"><img src="public/img/twitter_share.png"></a>
+			<a class="grey-text text-lighten-4 right share-img-footer" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Faoapao.com" target="_blank"><img src="public/img/facebook_share.png"></a>
+			<div class="grey-text text-lighten-4 right">Share on: </div>
+		</div>
+	</footer>
