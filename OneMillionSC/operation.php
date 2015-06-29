1e5e3f9ea0d02fc8d266838ab534d401e4b2706b
DonPablo <paserv@gmail.com>
2015-06-29 16:20:08 +0200

    a

---
+++ OneMillionSC/operation.php
@@ -0,0 +1,38 @@
+<?php session_start(); 
+require_once 'autoload.php';
+autoload (); 
+$controller = new Controller ();
+$excep = new CustomException(); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>One Million Social Club - Home Page</title>
+		<link type="text/css" rel="stylesheet" href="public/css/materialize.min.css"  media="screen,projection"/>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link type="text/css" rel="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		<link type="text/css" rel="stylesheet" href="public/css/omsc_.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+		<meta property="og:title" content="OMSC">
+		<meta property="og:image" content="http://www.aoapao.com/public/img/find.png">
+		<meta property="og:site_name" content="One Million Social Club">
+		<meta property="og:description" content="Join One Million Social Club">
+		<meta property="og:locale" content="en_UK">
+		<script type="text/javascript" src="public/js/jquery-2.1.3.min.js"></script>
+		<script type="text/javascript" src="public/js/jquery-ui-1.11.4.js"></script>
+		<script type="text/javascript" src="public/js/materialize.min.js"></script>
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.19&libraries=places&language=en"></script>
+		<script type="text/javascript" src="public/js/index.js"></script>
+		<script type="text/javascript" src="public/js/config.js"></script>
+	</head>
+
+<body>
+	<?php include 'header_.php'; ?>
+	
+	<?php if ($excep->existProblem) {
+		} else {
+	?>
+	CONTENT
+	<?php } include 'footer_.php'; ?>
+</body>
+
+</html>
